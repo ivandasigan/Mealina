38e8af73a739f9db740e619ed9d1904b3b632bb1
ivandasigan <51710349+ivandasigan@users.noreply.github.com>
2021-12-19 17:36:02 +0800

    Added child stackview

--- Mealina/Controllers/MealIngredientsViewController.swift
+++ Mealina/Controllers/MealIngredientsViewController.swift
@@ -15,21 +15,46 @@ class MealIngredientsViewController: UIViewController {
     @IBOutlet weak var others: UILabel!
     @IBOutlet weak var instruction: UILabel!
     @IBOutlet weak var stackView: UIStackView!
-    @IBOutlet weak var instructionText: UILabel! {
-        didSet {
-      
-        }
-    }
+    @IBOutlet weak var countItems: UILabel!
+  
     @IBOutlet weak var instructionContainerView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
     
         mealImage.layer.cornerRadius = 8
         mealImage.clipsToBounds = true
+        
+        for _ in 0..<10  {
+            addChildrenStackViews()
+        }
     }
     
 
-
+    private func addChildrenStackViews() {
+       
+        var ingredientImage = UIImageView(image: UIImage(named: "meal")!)
+        var ingredientName = UILabel()
+        ingredientName.text = "Tiktokerist"
+        ingredientName.font = .systemFont(ofSize: 16)
+        ingredientName.textColor = .darkGray
+        var ingredientContent = UILabel()
+        ingredientContent.text = "20grams"
+        ingredientContent.font = .systemFont(ofSize: 16)
+        ingredientContent.textColor = .darkGray
+        NSLayoutConstraint.activate([
+            ingredientImage.widthAnchor.constraint(equalToConstant: 40),
+            ingredientImage.heightAnchor.constraint(equalToConstant: 40)
+        ])
+        let innerStackView = UIStackView(arrangedSubviews: [ingredientImage, ingredientName])
+        let itemStackView = UIStackView(arrangedSubviews: [innerStackView, ingredientContent])
+        itemStackView.axis = .horizontal
+        innerStackView.axis = .horizontal
+        innerStackView.spacing = 20
+        itemStackView.distribution = .equalSpacing
+        stackView.addArrangedSubview(itemStackView)
+        
+    }
 
 }
